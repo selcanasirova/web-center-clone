a155993edd7bcdbc1f1d6e8b3d7997b1112d1157
Selcan <selcanasirova@gmail.com>
2024-05-09 17:37:26 +0400

    webcenter

---
+++ data/data.js
@@ -0,0 +1,147 @@
+[
+    {
+        "name":      "Mednova",
+        "img":       "https://webcenter.az/uploads/65a5e627cb7b48deafbb2e7f0ab2b523052ad3b2bd8f0.jpg",
+        "catagory":  "korporativ",
+        "link":      "https://mednova.az/"
+    },
+    {
+        "name":      "Yoshiro Electronics",
+        "img":       "https://webcenter.az/uploads/65a5ab99d4fb59697e29dde7ad13cf020f1737a7a7f36.jpg",
+        "catagory":  "online",
+        "link":      "https://yoshiroelectronics.com/en/"
+    },
+    {
+        "name":      "Alfa Print",
+        "img":       "https://webcenter.az/uploads/65a5ac67da39f53a0af842019bc4149fe9061c1eb19f6.jpg",
+        "catagory":  "korporativ",
+        "link":      "https://kataloq.gomap.az/az/all-poi/finance/business/8cdd2e08d56611e0ad4900226424597d"
+    },
+    {
+        "name":      "Uptime Marketing",
+        "img":       "https://webcenter.az/uploads/65a5e1a586c8b2af60c006b23fd0236265c028ed67b8b.jpg",
+        "catagory":  "korporativ",
+        "link":      "https://uptime-marketing.com/"
+    },
+    {
+        "name":      "Medsol",
+        "img":       "https://webcenter.az/uploads/65a5e47849ed09cca3af6597720f80528e8f4a434ffd4.jpg",
+        "catagory":  "korporativ",
+        "link":      "https://medsol.az/az/"
+    },
+    {
+        "name":      "Next Supplies",
+        "img":       "https://webcenter.az/uploads/65a5e72a7e92963a16e35cc6acab35538377ed00d9fde.jpg",
+        "catagory":  "korporativ",
+        "link":      "https://www.nextsupplies.az/about/"
+    },
+    {
+        "name":      "Certus",
+        "img":       "https://webcenter.az/uploads/620ac8db61fb4_tmb.jpg",
+        "catagory":  "korporativ",
+        "link":      "https://certus.az/"
+    },
+    {
+        "name":      "Atexa Group",
+        "img":       "https://webcenter.az/uploads/5f41bf092d2f7_tmb.jpg",
+        "catagory":  "korporativ",
+        "link":      "https://atexa.az/"
+    },
+    {
+        "name":      "Rulevoy.az",
+        "img":       "https://webcenter.az/uploads/65a5eae3c6445edeb31cf1d0694274d45729d9eab0c89.jpg",
+        "catagory":  "online",
+        "link":      "https://rulavoy.az/"
+    },
+    {
+        "name":      "LiderTech",
+        "img":       "https://webcenter.az/uploads/620ac7e14dcf2_tmb.jpg",
+        "catagory":  "online",
+        "link":      "https://lidertech.az/"
+    },
+    {
+        "name":      "Çin Dostu",
+        "img":       "https://webcenter.az/uploads/65a5aec153feb20bbc4bf193f2f131aff4690fd056e64.jpg",
+        "catagory":  "online",
+        "link":      "https://chindostu.az/"
+    },
+    {
+        "name":      "Nirlat Boya",
+        "img":       "https://webcenter.az/uploads/65a5aab7620967378c62bacd71362926ccf7139df4843.jpg",
+        "catagory":  "online",
+        "link":      "https://nirlat.az/az/"
+    },
+    {
+        "name":      "Kaspi Liseyi",
+        "img":       "https://webcenter.az/uploads/65a5c5c90be22a0d55bb2b74aa5d0b3b39da6a72cea3d.jpg",
+        "catagory":  "diger",
+        "link":      "https://kaspiliseyi.az/az/"
+    },
+    {
+        "name":      "CocaCola İlə Fərq Yarat",
+        "img":       "https://webcenter.az/uploads/5e9b8f732fa8b_tmb.jpg",
+        "catagory":  "diger",
+        "link":      "https://www.coca-cola.com/az/az"
+    },
+    {
+        "name":      "Tech.az",
+        "img":       "https://webcenter.az/uploads/65a6939df0a35beef57988a993f341f84e12467ab18fb.jpg",
+        "catagory":  "diger",
+        "link":      "https://tech.az/az/"
+    },
+    {
+        "name":      "HrAgenda Forum",
+        "img":       "https://webcenter.az/uploads/620ac86d9ca87_tmb.jpg",
+        "catagory":  "online",
+        "link":      "https://hragenda.az/"
+    },
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    }
+    ,
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    },
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    },
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    },
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    },
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    },
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    },
+    {
+        "name":      "",
+        "img":       "",
+        "catagory":  "",
+        "link":      ""
+    }
+]
